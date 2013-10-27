646742e07f94b19bef19abf14feb457f5b989839
Robbins Cleozier <rcleozier@gmail.com>
2013-10-26 20:44:27 -0400

    Update

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 var url = window.location.href;
 
 if (url.indexOf("localhost") != -1) {
-	var serviceURL = "http://localhost/dev/restock/site/public";
+	var serviceURL = "http://localhost/dev/soleinsider/site/public";
 } else{
 	var serviceURL = "http://soleinsider.com/public";
 }
