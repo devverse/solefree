3e1f433702b562aabe9900694701527eff5fb209
Devverse <Devverse@gmail.com>
2014-06-30 01:29:52 -0400

    Added news back into app

---
+++ app/controllers/news_c.js
@@ -0,0 +1,20 @@
+function newsController($scope, $rootScope,$location,$filter,news_service)
+{
+	$scope.news = [];
+
+  $scope.getNews = function(){
+    $scope.showLoading = true;
+      news_service.getFeeds().then(
+        function(data) {
+            $scope.news = data;
+        }, function(err) {
+            alert(err);
+        });
+  };
+
+  $scope.init = (function ()
+  {
+    $scope.getNews();
+    $rootScope.$emit("featured", false);
+  })();
+}
---
+++ app/models/news_service.js
@@ -0,0 +1,39 @@
+soleinsiderApp.factory('news_service', ['$rootScope', '$q', '$http', function($rootScope, $q, $http) {
+
+	var api = soleinsider.base_url;
+
+	self.makePost = function (endpoint, post) {
+
+        post = (!post) ? {} : post;
+        if(!endpoint) {
+            window.alert("Could not connect to database");
+            return;
+        }
+
+        var deferred = $q.defer();
+        $http.post(api + endpoint, post).success(function (data) {
+            if(data) {
+                if(data == 'false') {
+                    data = [];
+                }
+                deferred.resolve(data);
+            } else {
+                deferred.reject("Data was rejected: " + post);
+            }
+        });
+        return deferred.promise;
+
+    };
+
+    self.getFeeds = function(){
+        return self.makePost('/mobile/rssFeeds');
+    };
+
+ 	return {
+
+        getFeeds : function(){
+            return self.getFeeds();
+        },
+	};
+
+}]);
