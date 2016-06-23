7e5c91cdba22bfc758fa7094a1a2f3f70e4d7acc
Robbins Cleozier <rcleozier@gmail.com>
2016-06-22 22:48:50 -0400

    Updated news layout

--- app/controllers/view_c.js
+++ app/controllers/view_c.js
@@ -6,10 +6,12 @@ function viewController($scope, $rootScope, news_service) {
   };
 
   $scope.init = (function() {
+    $("body").scrollTop();
     $scope.loadArticle();
     $rootScope.$emit("featured", false);
     $rootScope.$emit("showback_button", true);
     window.showBannerAd();
+    window.randomInterstitial();
   })();
 }
 
