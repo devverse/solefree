a0188f192329247b0595d320dbdfb37fd657b4e2
Robbins Cleozier <rcleozier@gmail.com>
2018-06-23 14:56:45 -0400

    Switching to ng-ifs and minor tweaks

--- app/controllers/releases_c.js
+++ app/controllers/releases_c.js
@@ -1,11 +1,10 @@
 function releasesController($scope, $rootScope, $filter, $location, release_service, menu_service) {
 
+  var last_product_id = false;
+
   $scope.coming = [];
-  $scope.last_product_id = false;
   $scope.releases = [];
   $scope.showmsg = false;
-  $scope.showerror = false;
-  $scope.errorMessage = "";
   $scope.show_loading = true;
   $scope.show_coming = false;
   $scope.scrollPosition = 0;
@@ -55,7 +54,6 @@ function releasesController($scope, $rootScope, $filter, $location, release_serv
       function(data) {
         $scope.releases = data;
         $scope.show_loading = false;
-        $scope.releaseAddedAlert(data);
       }, function(err) {
         alert(err);
       });
@@ -89,62 +87,13 @@ function releasesController($scope, $rootScope, $filter, $location, release_serv
       product.releasing_today = false;
     }
 
-    if (product.coming_soon === '1' && $scope.last_product_id != product.id) {
-      $scope.last_product_id = product.id
+    if (product.coming_soon === '1' &&  last_product_id != product.id) {
+      last_product_id = product.id
       $scope.coming.push(product);
-    } else if ($scope.last_product_id == product.id) {
+    } else if (last_product_id == product.id) {
       return;
     } else {
       return product;
-    }
-  };
-
-  $scope.addReminder = function(product) {
-    member_id = localStorage.getItem("member_id");
-    if (member_id == "false" || member_id == 0 || member_id == null) {
-      $scope.showerror = true;
-      $scope.errorMessage = "You must be logged for reminders";
-      return;
-    } else {
-      $scope.showerror = true;
-      $scope.errorMessage = "Reminder added for " + product.name;
-    }
-
-    release_service.addReminder(product, member_id).then(
-      function(data) {
-        $scope.showmsg = true;
-        $scope.sneakerName = product.name;
-        toastr.success("Reminder added for " + product.name);
-      },
-      function(err) {
-        alert(err);
-      }
-    );
-  };
-
-  $scope.releaseAddedAlert = function(data) {
-
-    if (localStorage.getItem('release-date-id') == null) {
-      localStorage.setItem('release-date-id', 4420);
-    }
-
-    var newReleases = 0;
-    var newHigh = 0;
-    var currentHigh = parseInt(localStorage.getItem('release-date-id'));
-
-    for (var key in data) {
-      for (var x = 0; x < data[key].length; x++) {
-        if (data[key][x].id > currentHigh) {
-          newHigh = data[key][x].id;
-          currentHigh = newHigh
-          newReleases++;
-        }
-      }
-    }
-
-    if (newReleases > 0) {
-      toastr.success(newReleases + " New Releases Added");
-      localStorage.setItem('release-date-id', newHigh);
     }
   };
 
@@ -154,19 +103,6 @@ function releasesController($scope, $rootScope, $filter, $location, release_serv
     $rootScope.$emit("showback_button", false);
     menu_service.handleMenu();
     // menu_service.handleSwiper();
-
-    // set initial limit to say 30.
-    $scope.renderLimit = 30;
-    // bind this function with directive.
-    $scope.updateLimit = function(value){
-      if(value == 'bottom'){
-        console.log('updateLimit xxx');
-        $scope.contValue += 1;
-        $scope.renderLimit += 30;
-        console.log('here!!');
-      }
-    };
-
   })();
 }
 
