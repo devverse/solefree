aaada82304e6561097602b67af3d1f0dc273966e
Robbins Cleozier <rcleozier@gmail.com>
2018-07-27 20:21:21 -0400

    Tweaks to splash and alerts

--- app/badge.js
+++ app/badge.js
@@ -1,7 +1,13 @@
 var badge = {
+  clear: function() {
+    return;
+  };
   increase: function() {
-
-  }
+    return;
+  };
+  set: function() {
+    return;
+  };
 }
 
 window.badge = badge;
@@ -9,6 +15,8 @@ window.badge = badge;
 document.addEventListener("deviceready", onDeviceReadyBadge, false);
 
 function onDeviceReadyBadge() {
-    window.badge = cordova.plugins.notification.badge;
-    cordova.plugins.notification.badge.configure({ autoClear: true });
+    cordova.plugins.notification.badge.requestPermission(function (granted) {
+      window.badge = cordova.plugins.notification.badge;
+      cordova.plugins.notification.badge.configure({ autoClear: true });
+    });
 }
