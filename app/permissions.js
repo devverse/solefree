f05d3bc08fc8ec67f10b900a055e1bea26fe3533
Robbins Cleozier <rcleozier@gmail.com>
2018-07-29 23:06:24 -0400

    cardova

--- app/permissions.js
+++ app/permissions.js
@@ -4,51 +4,28 @@ function onDeviceReady() {
 
   var permissions = cordova.plugins.permissions;
 
-  function successCallback() {
+  var list = [
+    permissions.VIBRATE,
+    permissions.READ_CALENDAR,
+    permissions.WRITE_CALENDAR,
+    permissions.ACCESS_NETWORK_STATE
+  ];
 
-  };
+  permissions.hasPermission(list, callback, null);
 
-  function errorCallback() {
+  function error() {
+    console.warn('Camera or Accounts permission is not turned on');
+  }
 
-  };
+  function success(status) {
+    if (!status.hasPermission) {
 
-  permissions.hasPermission(permissions.VIBRATE, function(status) {
-    if (status.hasPermission) {
-      alert("VIBRATE");
-    } else {
-      permissions.requestPermissions(permissions.VIBRATE, successCallback, errorCallback);
+      permissions.requestPermissions(
+        list,
+        function(status) {
+          if (!status.hasPermission) error();
+        },
+        error);
     }
-  });
-
-  permissions.hasPermission(permissions.READ_CALENDAR, function(status) {
-    if (status.READ_CALENDAR) {
-      alert("READ_CALENDAR");
-    } else {
-      permissions.requestPermissions(permissions.READ_CALENDAR, successCallback, errorCallback);
-    }
-  });
-
-  permissions.hasPermission(permissions.WRITE_CALENDAR, function(status) {
-    if (status.WRITE_CALENDAR) {
-      alert("WRITE_CALENDAR");
-    } else {
-      permissions.requestPermissions(permissions.WRITE_CALENDAR, successCallback, errorCallback);
-    }
-  });
-
-  permissions.hasPermission(permissions.ACCESS_NETWORK_STATE, function(status) {
-    if (status.ACCESS_NETWORK_STATE) {
-      alert("ACCESS_NETWORK_STATE");
-    } else {
-      permissions.requestPermissions(permissions.ACCESS_NETWORK_STATE, successCallback, errorCallback);
-    }
-  });
-
-  permissions.hasPermission(permissions.ACCESS_NOTIFICATION_POLICY, function(status) {
-    if (status.ACCESS_NOTIFICATION_POLICY) {
-      alert("ACCESS_NOTIFICATION_POLICY");
-    } else {
-      permissions.requestPermissions(permissions.ACCESS_NOTIFICATION_POLICY, successCallback, errorCallback);
-    }
-  });
+  }
 }
