1dab0d0c042fc12374283530fb974cae33aa2e21
Robbins Cleozier <rcleozier@gmail.com>
2018-07-29 22:48:50 -0400

    Permissions refactor

--- dist/cardova.concat.js
+++ dist/cardova.concat.js
@@ -144,17 +144,54 @@ function onDeviceReadySplash() {
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-  alert('onDeviceReady 2nd');
 
   var permissions = cordova.plugins.permissions;
 
-  alert(permissions);
-  permissions.hasPermission(permissions.VIBRATE, function( status ){
-    if ( status.hasPermission ) {
-      alert("Yes :D ");
+  function successCallback() {
+
+  };
+
+  function errorCallback() {
+
+  };
+
+  permissions.hasPermission(permissions.VIBRATE, function(status) {
+    if (status.hasPermission) {
+      alert("VIBRATE");
+    } else {
+      permissions.requestPermissions(permissions.VIBRATE, successCallback, errorCallback);
     }
-    else {
-      alert("No :( ");
+  });
+
+  permissions.hasPermission(permissions.READ_CALENDAR, function(status) {
+    if (status.READ_CALENDAR) {
+      alert("READ_CALENDAR");
+    } else {
+      permissions.requestPermissions(permissions.READ_CALENDAR, successCallback, errorCallback);
+    }
+  });
+
+  permissions.hasPermission(permissions.WRITE_CALENDAR, function(status) {
+    if (status.WRITE_CALENDAR) {
+      alert("WRITE_CALENDAR");
+    } else {
+      permissions.requestPermissions(permissions.WRITE_CALENDAR, successCallback, errorCallback);
+    }
+  });
+
+  permissions.hasPermission(permissions.ACCESS_NETWORK_STATE, function(status) {
+    if (status.ACCESS_NETWORK_STATE) {
+      alert("ACCESS_NETWORK_STATE");
+    } else {
+      permissions.requestPermissions(permissions.ACCESS_NETWORK_STATE, successCallback, errorCallback);
+    }
+  });
+
+  permissions.hasPermission(permissions.ACCESS_NOTIFICATION_POLICY, function(status) {
+    if (status.ACCESS_NOTIFICATION_POLICY) {
+      alert("ACCESS_NOTIFICATION_POLICY");
+    } else {
+      permissions.requestPermissions(permissions.ACCESS_NOTIFICATION_POLICY, successCallback, errorCallback);
     }
   });
 }
